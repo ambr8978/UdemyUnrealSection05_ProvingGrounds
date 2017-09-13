3eded8c372c789c3ad4f386cbb8757ab0acec069
DESKTOP-96GJUGK\antho <anthony.nicolosi78@gmail.com>
2017-09-13 12:31:52 -0600

    Completed major refactor or directory structures and filenames. Reparented first person character and projectile BPs to take newly renamed counterparts. Fixed skeleton target issues after refactor for various NPC animations. Lecture 236.

--- Source/S05_ProvingGrounds/PatrolRouteComponent.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PatrolRouteComponent.h"
-
-
-TArray<AActor*> UPatrolRouteComponent::GetPatrolPoints() const
-{
-	return PatrolPoints;
-}
--- Source/S05_ProvingGrounds/S05_ProvingGroundsGameMode.cpp
+++ Source/S05_ProvingGrounds/S05_ProvingGroundsGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "S05_ProvingGroundsGameMode.h"
 #include "S05_ProvingGroundsHUD.h"
-#include "S05_ProvingGroundsCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AS05_ProvingGroundsGameMode::AS05_ProvingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
